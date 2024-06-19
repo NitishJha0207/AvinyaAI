eda6bc5dd6f94817a90f5d884ca5350f52ae07b3
NitishJha0207 <nitishj639@gmail.com>
2024-06-19 23:34:16 +0530

    IMproved login_view file

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -64,10 +64,19 @@ class _LoginViewState extends State<LoginView> {
                 email: email, 
                 password: password
                 );
-                Navigator.of(context).  pushNamedAndRemoveUntil(
-                  mainuiRoute, 
+                final user = FirebaseAuth.instance.currentUser;
+                if (user?.emailVerified ?? false){
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                      mainuiRoute, 
+                      (route) => false ,
+                    );
+                } else {
+                  Navigator.of(context).  pushNamedAndRemoveUntil(
+                  verifyEmailRoute, 
                   (route) => false ,
                   );
+                }
+                
               } 
               on FirebaseAuthException catch(e){
                 if (e.code== 'invalid-credential'){
